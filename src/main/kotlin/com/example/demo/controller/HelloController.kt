851d1ae4fa98aebae946ea3f3bbf7a54b7da7832
ufoscout <ufoscout@gmail.com>
2017-07-05 14:05:36 +0200

    FixTests

--- src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -1,5 +1,7 @@
 package com.example.demo.controller
 
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.reactive.publish
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
 
     @GetMapping("/hello/{name}")
-    fun get(@PathVariable name: String) = "Hello, $name"
+    fun get(@PathVariable name: String) = publish(Unconfined) {send("Hello, $name")}
 
 }
